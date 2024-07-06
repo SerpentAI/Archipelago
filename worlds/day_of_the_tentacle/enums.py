c3bba05277dbbba69d964c9397d7d731af800c05
Nicholas Brochu <info@nicholasbrochu.com>
2024-07-06 09:58:00 -0400

    wip

---
+++ worlds/day_of_the_tentacle/enums.py
@@ -0,0 +1,104 @@
+import enum
+
+
+class DayOfTheTentacleEvents(enum.Enum):
+    pass
+    # CHARON_CALLED = "Event: Charon Called"
+
+
+class DayOfTheTentacleGoals(enum.Enum):
+    pass
+
+
+class DayOfTheTentacleItems(enum.Enum):
+    BATTERY = "Battery"
+    BLANKET = "Blanket"
+    BOOBOO_B_GONE = "BooBoo-B-Gone"
+    BOWLING_BALL = "Bowling Ball"
+    BOX_O_LAUGHS = "Box O' Laughs"
+    BRUSH = "Brush"
+    BUCKET = "Bucket"
+    BUCKET_FULL_OF_SOAPY_WATER = "Bucket Full of Soapy Water"
+    BUCKET_FULL_OF_WATER = "Bucket Full of Water"
+    CAN_OPENER = "Can Opener"
+    CAT = "Cat"
+    CHATTERING_TEETH = "Chattering Teeth"
+    CIGAR_LIGHTER = "Cigar Lighter"
+    COFFEE = "Coffee"
+    COLD_WET_HAMSTER = "Cold Wet Hamster"
+    CONTRACT = "Contract"
+    CROWBAR = "Crowbar"
+    DECAF_COFFEE = "Decaf Coffee"
+    DENTURES = "Dentures"
+    DIME = "Dime"
+    DINNER_CERTIFICATE = "Dinner Certificate"
+    DISAPPEARING_INK = "Disappearing Ink"
+    DUSTY_WARM_HAMSTER = "Dusty Warm Hamster"
+    EXPLODING_CIGAR = "Exploding Cigar"
+    EXTENSION_CORD = "Extension Cord"
+    FAKE_BARF = "Fake Barf"
+    FLAG = "Flag"
+    FLAG_GUN = "Flag Gun"
+    FLIER = "Flier"
+    FORK = "Fork"
+    FROZEN_HAMSTER = "Frozen Hamster"
+    FUNNEL = "Funnel"
+    GOLD_PLATED_QUILL_PEN = "Gold-Plated Quill Pen"
+    GUM_WITH_A_DIME_STUCK_IN_IT = "Gum with a Dime Stuck in It"
+    HAMSTER = "Hamster"
+    HELP_WANTED_SIGN = "Help Wanted Sign"
+    KEYS = "Keys"
+    LAB_COAT = "Lab Coat"
+    LEFT_HANDED_HAMMER = "Left-Handed Hammer"
+    LETTER = "Letter"
+    NAME_TAG = "Name Tag"
+    OIL = "Oil"
+    PATENT_APPLICATION = "Patent Application"
+    QUARTERS = "Quarters"
+    RED_PAINT = "Red Paint"
+    ROLLER_SKATES = "Roller Skates"
+    ROPE = "Rope"
+    SCALPEL = "Scalpel"
+    SIGNED_CONTRACT = "Signed Contract"
+    SOAP = "Soap"
+    SPAGHETTI = "Spaghetti"
+    SQUEAKY_MOUSE_TOY = "Squeaky Mouse Toy"
+    STAMP = "Stamp"
+    STAMP_ALBUM = "Stamp Album"
+    SWEATER = "Sweater"
+    SWISS_BANKBOOK = "Swiss Bankbook"
+    TENTACLE_CHART = "Tentacle Chart"
+    TEXTBOOK = "Textbook"
+    TOASTY_WARM_HAMSTER = "Toasty Warm Hamster"
+    TROPHY = "Trophy"
+    VIDEOTAPE = "Videotape"
+    VINEGAR = "Vinegar"
+    WAD_OF_GUM = "Wad of Gum"
+    WET_SOGGY_NOODLES = "Wet Soggy Noodles"
+    WINE_BOTTLE = "Wine Bottle"
+
+
+class DayOfTheTentacleLocations(enum.Enum):
+    pass
+
+
+class DayOfTheTentacleRegions(enum.Enum):
+    PRESENT_LOBBY = "Lobby (Present)"  # 34
+
+
+class DayOfTheTentacleTags(enum.Enum):
+    pass
+
+
+# class ZorkGrandInquisitorTags(enum.Enum):
+#     CORE = "Core"
+#     DEATHSANITY = "Deathsanity"
+#     FILLER = "Filler"
+#     HOTSPOT = "Hotspot"
+#     INVENTORY_ITEM = "Inventory Item"
+#     MISSABLE = "Missable"
+#     SPELL = "Spell"
+#     SUBWAY_DESTINATION = "Subway Destination"
+#     TELEPORTER_DESTINATION = "Teleporter Destination"
+#     TOTEMIZER_DESTINATION = "Totemizer Destination"
+#     TOTEM = "Totem"
