ff3d07514eb54370a9a1953429b1e1b1df793cb3
Nicholas Brochu <info@nicholasbrochu.com>
2024-08-16 11:50:59 -0400

    WIP

---
+++ worlds/smb3/data/enemizer_data.py
@@ -0,0 +1,48 @@
+from ..enums import SMB3Enemies
+
+enemizer_swaps = {
+    SMB3Enemies.ANGRY_SUN: (
+        SMB3Enemies.ANGRY_SUN,
+        SMB3Enemies.FIRE_CHOMP,
+        SMB3Enemies.LAKITU,
+    ),
+    SMB3Enemies.BIG_BERTHA: (
+        SMB3Enemies.BABY_CHEEP_CHEEP,
+        SMB3Enemies.BIG_BERTHA,
+        SMB3Enemies.BLOOPER,
+        SMB3Enemies.BLOOPER_FAMILY,
+        SMB3Enemies.BLOOPER_NANNY,
+        SMB3Enemies.LAVA_LOTUS,
+        SMB3Enemies.SPIKY_CHEEP_CHEEP,
+    ),
+    SMB3Enemies.BLOOPER: (
+        SMB3Enemies.BABY_CHEEP_CHEEP,
+        SMB3Enemies.BIG_BERTHA,
+        SMB3Enemies.BLOOPER,
+        SMB3Enemies.BLOOPER_FAMILY,
+        SMB3Enemies.BLOOPER_NANNY,
+        SMB3Enemies.LAVA_LOTUS,
+        SMB3Enemies.SPIKY_CHEEP_CHEEP,
+    ),
+    SMB3Enemies.BLOOPER_FAMILY: (
+        SMB3Enemies.BABY_CHEEP_CHEEP,
+        SMB3Enemies.BIG_BERTHA,
+        SMB3Enemies.BLOOPER,
+        SMB3Enemies.BLOOPER_FAMILY,
+        SMB3Enemies.BLOOPER_NANNY,
+        SMB3Enemies.LAVA_LOTUS,
+        SMB3Enemies.SPIKY_CHEEP_CHEEP,
+    ),
+    SMB3Enemies.BLOOPER_NANNY: (
+        SMB3Enemies.BABY_CHEEP_CHEEP,
+        SMB3Enemies.BIG_BERTHA,
+        SMB3Enemies.BLOOPER,
+        SMB3Enemies.BLOOPER_FAMILY,
+        SMB3Enemies.BLOOPER_NANNY,
+        SMB3Enemies.LAVA_LOTUS,
+        SMB3Enemies.SPIKY_CHEEP_CHEEP,
+    ),
+    SMB3Enemies.BOB_OMB: (
+        
+    ),
+}
