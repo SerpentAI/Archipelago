cb457d6842c55de12210ac016ecc07f0c71c85d0
Silvris <58583688+Silvris@users.noreply.github.com>
2024-12-25 01:41:48 -0600

    first pass

--- worlds/keymasters_keep/games/__init__.py
+++ worlds/keymasters_keep/games/__init__.py
@@ -10,8 +10,10 @@ from .archipelago_multiworld_randomizer_game import (
 )
 
 from .pinball_fx3_game import PinballFX3Game, PinballFX3ArchipelagoOptions
+from .rabbit_and_steel_game import RabbitAndSteelGame, RabbitAndSteelArchipelagoOptions
 from .street_fighter_6_game import StreetFighter6Game, StreetFighter6ArchipelagoOptions
 from .trackmania_game import TrackmaniaGame, TrackmaniaArchipelagoOptions
+
 
 # Metagame Imports
 from .game_backlog_game import GameBacklogGame, GameBacklogArchipelagoOptions
@@ -20,6 +22,7 @@ from .game_backlog_game import GameBacklogGame, GameBacklogArchipelagoOptions
 games: Dict[str, Type[Game]] = {
     ArchipelagoMultiworldRandomizerGame.game_name_with_platforms(): ArchipelagoMultiworldRandomizerGame,
     PinballFX3Game.game_name_with_platforms(): PinballFX3Game,
+    RabbitAndSteelGame.game_name_with_platforms(): RabbitAndSteelGame,
     StreetFighter6Game.game_name_with_platforms(): StreetFighter6Game,
     TrackmaniaGame.game_name_with_platforms(): TrackmaniaGame,
 }
@@ -34,6 +37,7 @@ class GameArchipelagoOptions(
     # Add in reverse alphabetical order
     TrackmaniaArchipelagoOptions,
     StreetFighter6ArchipelagoOptions,
+    RabbitAndSteelArchipelagoOptions,
     PinballFX3ArchipelagoOptions,
     GameBacklogArchipelagoOptions,
     ArchipelagoMultiworldRandomizerArchipelagoOptions,
